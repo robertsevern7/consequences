06f4940b00e684a884c4970494848f6358c76f84
Rob and Bu <Rob and Bu@RobandBu-HP>
2012-04-08 17:32:32 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var store = require('./routes/store');
+
+var app = module.exports = express.createServer();
+app.use(express.cookieParser());
+app.use(express.session({ secret: "keyboard cat" }));
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', store.home);
+app.post('/', store.home_post_handler);
+app.get('/items', store.items);
+app.get('/item/:id', store.item);
+app.get('/page', store.page);
+
+app.get('/logout', function(req, res) {
+    delete req.session.username;
+    res.redirect('/');
+});
+
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ routes/store.js
@@ -0,0 +1,41 @@
+exports.home = function(req, res) {
+    if (typeof req.session.username == 'undefined') res.render('home', { title: 'Ninja Store'});
+    else res.redirect('/items');
+};
+
+exports.home_post_handler = function(req, res) {
+  var username = req.body.username || 'Anonymous';
+  req.session.username = username;
+  res.redirect('/');
+};
+
+var items = {
+    SKN:{name:'Shuriken', price:100},
+    ASK:{name:'Ashiko', price:690},
+    CGI:{name:'Chigiriki', price:250},
+    NGT:{name:'Naginata', price:900},
+    KTN:{name:'Katana', price:1000}
+};
+
+exports.items = function(req, res) {
+    if (typeof req.session.username == 'undefined') res.redirect('/');
+    else res.render('items', { title: 'Ninja Store - Items', username: req.session.username, items:items });
+};
+
+exports.item = function(req, res) {
+    if (typeof req.session.username == 'undefined') res.redirect('/');
+    else {
+        var name = items[req.params.id].name;
+        var price = items[req.params.id].price;
+        res.render('item', { title: 'Ninja Store - ' + name, username: req.session.username, name:name, price:price });
+    }
+};
+
+exports.page = function(req, res) {
+    var name = req.query.name;
+    var contents = {
+        about: 'Ninja Store sells the coolest ninja stuff in the world. Anyone shopping here is cool.',
+        contact: 'You can contact us at <address><strong>Ninja Store</strong>,<br>1, World Ninja Headquarters,<br>Ninja Avenue,<br>NIN80B7-JP,<br>Nihongo.</address>'
+    };
+    res.render('page', { title: 'Ninja Store - ' + name, username: req.session.username, content:contents[name] });
+};
