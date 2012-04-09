b66938c90bf32cb303c75646e7494c5ebdeb3230
Rob and Bu <Rob and Bu@RobandBu-HP>
2012-04-09 11:07:46 -0700

    Adding loggedout class that is hidden when logged in. Don't hide see others ever

--- public/javascripts/facebookLogin.js
+++ public/javascripts/facebookLogin.js
@@ -47,11 +47,11 @@ function FacebookWrapper() {
         if (loggedIn) {
             showAccountInfo();
             $('.loggedon').show();
+            $('.loggedout').hide();
         } else {
             showLoginButton();
             $('.loggedon').hide();
-        }
-        
-        
+            $('.loggedout').show();
+        }  
     }
 }
