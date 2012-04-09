b14d24b0b04447f470eae19c00c3a6e1bba21670
Rob and Bu <Rob and Bu@RobandBu-HP>
2012-04-09 14:57:56 -0700

    Remaining characters check on the story

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -10,4 +10,23 @@ $(document).ready(function() {
         change: setSliderValueText,
         slide: setSliderValueText
     });
+    
+    function checkRemainingCharacters() {
+        var totalChars = (this.value || $("#openingparagraph")[0].value).length;
+        var MAX_CHARS = 280;
+        var remaining = MAX_CHARS - totalChars;
+        
+        var charWarning = $("#charwarning");
+        if (remaining >= 0) {
+            charWarning.removeClass('gonebad');
+            charWarning.text(remaining + ' characters remaining');
+        } else {
+            charWarning.addClass('gonebad');
+            charWarning.text((-remaining) + ' characters too many');
+        }
+    }
+    
+    checkRemainingCharacters()
+    $("#openingparagraph").blur(checkRemainingCharacters);
+    $("#openingparagraph").keyup(checkRemainingCharacters);
 })
