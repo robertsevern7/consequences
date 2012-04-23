e73239105ef4320bca94a9239727959d0a9b4e7c
Rob and Bu <Rob and Bu@RobandBu-HP>
2012-04-22 18:54:49 -0700

    Save the user stories - associates with a user and saves the story. Still need to save the sections

--- routes/store.js
+++ routes/store.js
@@ -1,5 +1,6 @@
 var facebookAuthenticate = require('facebookAuthenticate')
-var sql = require('mysql_connector')
+//var sql = require('mysql_connector')
+var sql = require('sequelize_connector')
 
 exports.home = function(req, res) {
     if (typeof req.session.username == 'undefined') res.render('home', { title: 'Consequences'});
@@ -28,54 +29,33 @@ exports.create_post_handler = function(req, res) {
             });
         } else {
             var story = {
-                user: 1,//req.body.user,
                 title: req.body.title,
                 characters: req.body.characters,
-                storySections: req.body.storySections,
-                content: req.body.content
+                max_sections: req.body.storySections//,
+                //content: req.body.content
             };
             
-            sql.makeQuery('INSERT INTO consequences.story(title, owner_id, max_sections) VALUES("' + story.title + '", "' + story.user + '", ' + story.storySections + ')', function(err) {
-                if (err) {
-                    console.log(err);
-                    res.send({
-                        success: false,
-                        user: story.user
-                    });
-                    return;
-                }
-                
-                sql.makeQuery('SELECT MAX(id) as lastId FROM consequences.story WHERE story.owner_id =' +story.user, function(err2, results, fields) {
-                    if (err2) {
-                        console.log(err2);
-                        res.send({
-                            success: false,
-                            user: story.user
-                        });
-                        return;
-                    }
-                    console.dir(results)
-                    console.dir(story.user)
-                    console.dir(story.content)
-                    console.log('INSERT INTO consequences.section(user_id, story_id, content) VALUES(' + story.user + ', ' + results[0].lastId + ', "' + story.content + '")')
-                    sql.makeQuery('INSERT INTO consequences.section(user_id, story_id, content) VALUES(' + story.user + ', ' + results[0].lastId + ', "' + story.content + '")', function(err3) {                    
-                        if (err3) {
-                            console.log(err3);
-                            res.send({
-                                success: false,
-                                user: story.user
-                            });
-                            return;
-                        }
-                        
-                        res.send({
-                            success: true,
-                            user: story.user,
-                            savedId: results.story
-                        });
-                    });
+            function failureHandler() {
+                res.send({
+                    success: false,
+                    user: story.user
                 });
-            })
+            }
+            
+            function sendSuccessResponse(userId, storyId) {
+                res.send({
+                    success: true,
+                    user: userId,
+                    savedId: storyId
+                });
+            }
+            
+            function saveStory(user) {                
+                sql.createStory(story, user, sendSuccessResponse, failureHandler);
+            }
+            
+            //No other logins that facebook at the moment
+            sql.createUser(req.body.user, 'FACEBOOK', saveStory, failureHandler);
         }
     }
     
