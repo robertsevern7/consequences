f1a5fbdf197b0502de0e471accfd1a6ee5e9abc4
Rob and Bu <Rob and Bu@RobandBu-HP>
2012-05-27 10:22:25 -0700

    Adding notification to user if they have a lock on the story or not. Countdown on screen. Also halfway done for creating a story seed

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.get('/create', store.create);
 app.post('/create', store.create_post_handler);
 app.post('/contribute', store.contribute_post_handler);
 app.post('/like', store.like_post_handler);
+app.post('/seed', store.getSeedPostHandler);
 app.get('/userstories/:page/:sortOrder/:sortDir', store.userStories);
 app.get('/friendsstories', store.friendsStories);
 app.post('/getfriends', store.friendsRetrieval);
